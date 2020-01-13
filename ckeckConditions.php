5998eb13b9285ed2b0335ab79b40e4e45256150c
Ashvin Patidar <59476864+ashupatidar123@users.noreply.github.com>
2020-01-13 17:41:25 +0530

    API or Input form value check validation
    API or Input form value check validation

---
+++ ckeckConditions.php
@@ -0,0 +1,29 @@
+<?php
+public function check_required_value($chk_params, $converted_array)
+{
+    foreach ($chk_params as $param)
+    {
+        if(array_key_exists($param, $converted_array) && ($converted_array[$param] !='')){
+            $check_error = 0;
+        } 
+        else{
+            $check_error=array('check_error'=>1,'param'=>$param);
+            break;
+        }
+    }
+    return $check_error;
+}
+
+
+
+public function getFollowers_post()
+{
+    $userid = $this->input->post('userid');
+    $checkData = array('userid' => $userid);
+    $required_parameter = array('userid');
+    $chk_error = $this->check_required_value($required_parameter,$checkData);
+    if ($chk_error) {
+        $resp = array('code'=>'501','message'=>'Missing '.ucwords($chk_error['param']));
+        @$this->response($resp); exit;
+    }
+}
